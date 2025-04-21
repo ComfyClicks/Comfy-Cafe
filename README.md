82bca58f573a49b6b34f44647571c58c651a45d7
ComfyClicks <79934542+ComfyClicks@users.noreply.github.com>
2025-04-21 01:48:44 -0400

    added README

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
+<div align="center">
+
 # Restaurant-Page
+
+## 🥐 🍩 👀 🍪 ☕️ 🥐 [Live Preview]() 🧁 🍩 👀 🍪 ☕️ 🧁
+
+<img src="./assets/screenshot.png">
+
+## Technologies Used 
+
+<img src="https://img.shields.io/badge/javascript-%23323330.svg?style=for-the-badge&logo=javascript&logoColor=%23F7DF1E">
+<img src="https://img.shields.io/badge/html5-%23E34F26.svg?style=for-the-badge&logo=html5&logoColor=white">
+<img src="https://img.shields.io/badge/css3-%231572B6.svg?style=for-the-badge&logo=css3&logoColor=white">
+<img src="https://img.shields.io/badge/webpack-%238DD6F9.svg?style=for-the-badge&logo=webpack&logoColor=black">
+
+</div>
--- webpack.config.js
+++ webpack.config.js
@@ -2,20 +2,29 @@ const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-  mode: "development",
+  mode: process.env.NODE_ENV === "production" ? "production" : "development",
   entry: "./src/index.js",
   output: {
-    filename: "main.js",
+    filename: "[name].[contenthash].js", // Contenthash for cache busting
     path: path.resolve(__dirname, "dist"),
+    publicPath: '', // Empty string for relative paths
     clean: true,
   },
-  devtool: "eval-source-map",
+  devtool: process.env.NODE_ENV === "production" ? "source-map" : "eval-source-map",
   devServer: {
     watchFiles: ["./src/template.html"],
+    hot: true,
+    open: true,
+    historyApiFallback: true, // For SPA routing
   },
   plugins: [
     new HtmlWebpackPlugin({
       template: "./src/template.html",
+      minify: process.env.NODE_ENV === "production" ? {
+        removeComments: true,
+        collapseWhitespace: true,
+        removeAttributeQuotes: true
+      } : false,
     }),
   ],
   module: {
@@ -29,9 +38,21 @@ module.exports = {
         loader: "html-loader",
       },
       {
-        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        test: /\.(png|svg|jpg|jpeg|gif|ico)$/i,
         type: "asset/resource",
+        generator: {
+          filename: 'assets/images/[name].[hash][ext]'
+        }
       },
     ],
+  },
+  optimization: {
+    minimize: process.env.NODE_ENV === "production",
+    splitChunks: {
+      chunks: 'all',
+    },
+  },
+  performance: {
+    hints: process.env.NODE_ENV === "production" ? "warning" : false,
   },
 };
